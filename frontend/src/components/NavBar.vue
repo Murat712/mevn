7981136a8757a29bbae7b2d720f4956724781f9b
Murat712 <murat712xyz@proton.me>
2025-11-20 21:28:16 +0300

    <

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <nav class="navbar navbar-expand-md custom-nav">
+      <div class="container">
+        <a href="#" class="navbar-brand">Bostorek</a>
+        <ul class="navbar nav">
+          <li class="nav-item">
+              <a href="" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">Books</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">Contact Us</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">Login</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">Register</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+.custom-nav{
+  background-color: #063547;
+  padding: 15px 0;
+}
+
+.navbar-brand{
+  padding: 0px;
+  margin: 0px;
+  color: #fff;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.nav-link{
+  padding: 10px 25px;
+  color: #fff;
+  text-align: center;
+}
+</style>
